eed2c60be34b81f6ce87cc082c612c2cf1ba928b
Frank Nankivell <franknankivell@gmail.com>
2017-12-18 20:19:00 +0000

    Customer.java created online with Bitbucket

---
+++ Customer.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+// currently setup to take one customer. Need to be able to take numerous Customers.
+public class Customer {
+		   //Private fields
+		   private String firstName;
+		   private String lastName;
+		   private String email;
+		   private static AtomicInteger ID_GENERATOR = new AtomicInteger(1000);
+		 
+		   //Constructor method
+		   public Customer(String firstName, String lastName, String email)
+		   {
+		     this.firstName = firstName;
+		     this.lastName = lastName;
+		     this.email = email;
+		   }
+		   public String getFirstName()
+		   {
+		     return firstName;
+		   }
+		 
+		   //Accessor for lastName
+		   public String getLastName()
+		   {
+		     return lastName;
+		   }
+		   // Accessor for email
+		   public String getEmail()
+		   { 
+		       return email;
+		   }
+		     
+		   // Accessor for ID generator
+		public static AtomicInteger getID_GENERATOR() {
+			return ID_GENERATOR;
+		}
+		// mutator for ID generator
+		public static void setID_GENERATOR(AtomicInteger iD_GENERATOR) {
+			ID_GENERATOR = iD_GENERATOR;
+		}
+		}
